6deac8642ed58a39c2bc005ba914bbf0dab18022
Kaizad Patel <50244730+kaizadp@users.noreply.github.com>
2023-11-29 17:37:23 -0800

    process data

--- Process_plate.R
+++ Process_plate.R
@@ -1,12 +1,36 @@
-library(readxl)
+
+# load packages
 library(tidyverse)
-library(data.table)
 theme_set(theme_bw())
 
 
-FILEPATH = ("/RStudio/CFU_plates/data")
-# PLATE MAP ----
-## load the plate map and use this as the sample key
+# load files
+map = read.csv("data/Cell OD plate layout_carbstore.csv")
+data = read.csv("data/CSC_R2A_plate.csv", skip = 27)
 
-map = read.csv("Cell OD plate layout_carbstore.csv")
+# process
+map_processed = 
+  map %>% 
+  pivot_longer(cols = -X, names_to = "number", values_to = "sample") %>% 
+  mutate(number = parse_number(number),
+         well = paste0(X, number)) %>% 
+  dplyr::select(well, sample) %>% 
+  filter(!grepl("SKIP", sample)) %>% 
+  separate(sample, sep = " ", into = c("sample_name", "rep"), remove = F) %>% 
+  mutate_all(as.character)
 
+data_processed = 
+  data %>% 
+  dplyr::select(-X, -`T..600`) %>% 
+  rename(time = Time) %>% 
+  mutate(time_hr = as.numeric(hms(time), units = "hours")) %>% 
+  mutate_all(as.numeric) %>% 
+  filter(!is.na(time_hr)) %>% 
+  pivot_longer(cols = -time_hr, names_to = "well", values_to = "abs_600") %>% 
+  left_join(map_processed) %>% 
+  drop_na()
+
+# graph
+data_processed %>% 
+  ggplot(aes(x = time_hr, y = abs_600, group = sample, color = sample_name))+
+  geom_line()
